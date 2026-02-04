c0df0223961c829e3e6ca009054e6de887c2d35a
barsa-pudasaini <barsapudasaini6@gmail.com>
2026-02-04 11:19:49 +0545

    chnagess

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -9,14 +9,19 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
     //apiKey: "AIzaSyCH4WfgDAJxXYXLV8pGE9E73IM9QwoBv1g",
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: "moviebookingsystem-7ea47.firebaseapp.com",
-    projectId: "moviebookingsystem-7ea47",
-    storageBucket: "moviebookingsystem-7ea47.firebasestorage.app",
-    messagingSenderId: "594023186764",
-    appId: "1:594023186764:web:12272c658e9cf4a0678fe9",
-    measurementId: "G-5C1ZRHZXQN"
+   // authDomain: "moviebookingsystem-7ea47.firebaseapp.com",
+   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    // projectId: "moviebookingsystem-7ea47",
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    // storageBucket: "moviebookingsystem-7ea47.firebasestorage.app",
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+   // messagingSenderId: "594023186764",
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+   // appId: "1:594023186764:web:12272c658e9cf4a0678fe9",
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+   // measurementId: "G-5C1ZRHZXQN"
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
